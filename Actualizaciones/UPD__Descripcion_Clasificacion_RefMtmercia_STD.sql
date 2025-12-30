7d7a2fa066e01ef5af38d6d77c7561f095d5360d
GaspaObito <gaspa.obito2003@gmail.com>
2025-12-30 16:51:36 -0500

    Default Caja Reg By User

--- Actualizaciones/UPD__Descripcion_Clasificacion_RefMtmercia_STD.sql
+++ Actualizaciones/UPD__Descripcion_Clasificacion_RefMtmercia_STD.sql
@@ -1,7 +1,16 @@
-update m
+USE SUPERTODO
+select m.codigo,m.DESCRIPCIO,a.DESCRIPCIO,m.CODLINEA,a.CODLINEA,m.CODSBLIN,a.CODSBLIN,
+CASE 
+        WHEN m.DESCRIPCIO = a.DESCRIPCIO THEN 1
+        ELSE 0
+    END AS SonIguales
+From MtMercia m
+Inner Join aaamtmercia$ a ON a.CODIGO=m.CODIGO
+
+/*update m
 set 
 	m.DESCRIPCIO = a.DESCRIPCIO,
 	m.CODLINEA = a.CODLINEA,
 	m.CODSBLIN = a.CODSBLIN
 From MtMercia m
-Inner Join AAAMtmercia$ a ON a.CODIGO=m.CODIGO
+Inner Join AAAMtmercia$ a ON a.CODIGO=m.CODIGO*/
--- Consulta_Diarias/VAL_RefMtmercia_RNY_STD.sql
+++ Consulta_Diarias/VAL_RefMtmercia_RNY_STD.sql
@@ -2,7 +2,7 @@ USE SUPERTODO
 --BUSCAR REFE
 select codigo,codbarras,descripcio from MtMercia
 where 
-DESCRIPCIO like'%ALTAGRACIANO%'
-AND DESCRIPCIO like'%BLANCO%'
+DESCRIPCIO like'%FORELLI%'
+AND DESCRIPCIO like'%SALTINES%'
 --COGIO ULTIMO
-select codigo,codbarras,descripcio from MtMercia ORDER BY CODIGO DESC
+--select codigo,codbarras,descripcio from MtMercia WHERE CODIGO like'A-%' ORDER BY CODIGO DESC
